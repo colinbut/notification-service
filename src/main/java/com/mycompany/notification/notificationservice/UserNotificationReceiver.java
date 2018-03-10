c0972e34cfbf98c29510b2302993d95b1f98ac95
Colin But <colinbut@users.noreply.github.com>
2018-03-10 11:48:13 +0000

    Message Receiver Bean

---
+++ src/main/java/com/mycompany/notification/notificationservice/UserNotificationReceiver.java
@@ -0,0 +1,25 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved. 
+ * |-------------------------------------------------
+ */
+package com.mycompany.notification.notificationservice;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserNotificationReceiver {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserNotificationReceiver.class);
+
+    @JmsListener(destination = "user-notification-queue")
+    public void receiveMessage(UserDto userDto){
+        LOG.info("Received user notification message from user-notification-queue");
+
+        // TODO here we're going to process however we like
+        // not going to implement this part - leaving it as future work to do
+    }
+}
