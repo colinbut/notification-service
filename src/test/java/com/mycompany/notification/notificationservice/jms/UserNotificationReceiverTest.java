68c29eead9d0d273ab48ae2c17b6a15ec84c107d
Colin But <colinbut@users.noreply.github.com>
2018-03-10 11:57:49 +0000

    commit the stub test implementation for UserNotificationReceiver

---
+++ src/test/java/com/mycompany/notification/notificationservice/jms/UserNotificationReceiverTest.java
@@ -0,0 +1,19 @@
+/*
+ * |-------------------------------------------------
+ * | Copyright © 2018 Colin But. All rights reserved.
+ * |-------------------------------------------------
+ */
+package com.mycompany.notification.notificationservice.jms;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class UserNotificationReceiverTest {
+
+    @Test
+    @Ignore
+    public void testReceiveMessage(){
+        throw new UnsupportedOperationException("Not Yet Implemented");
+        // TODO fill in the test once you implement the Message Driven POJO functionality
+    }
+}
